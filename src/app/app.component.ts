7b7c35b86b066760fc350dedb134b409abef4738
john@work <johninquest@gmail.com>
2020-06-23 12:40:05 +0200

    add to initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { CalculatorService } from './services/calculator.service';
+
+export interface Vatdesc {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tax-app';
+  
+  constructor(private cs: CalculatorService) {}
+
+  title = 'a simple tax app';
+  inputAmount = new FormControl('', [Validators.required]);
+  inputTax = new FormControl('', [Validators.required]);
+  taxType = new FormControl('');
+
+  dateToday = new Date();
+
+  vats: Vatdesc[] = [
+    {value: 'fVat', viewValue: 'Forward VAT'},
+    {value: 'rVat', viewValue: 'Reverse VAT'}
+  ];
+
+  displayTaxType(tType: string) {
+    if(tType === 'fVat') {
+      return 'Forward VAT';
+    }else if(tType === 'rVat') {
+      return 'Reverse VAT';
+    }
+  }
+
+  showCalculatedTax() {
+    let amt: number = this.inputAmount.value;
+    let tax: number = this.inputTax.value;
+    let ttype: string = this.taxType.value;
+    if(amt && tax && ttype) {
+      return this.cs.calculatedTaxTotal(amt, tax, ttype);
+    }else {
+      return '';
+    }
+  }
+
+  showCalculatedTotal() {
+    let amt: number = this.inputAmount.value;
+    let tax: number = this.inputTax.value;
+    let ttype: string = this.taxType.value;
+    if(amt && tax && ttype) {
+      return this.cs.calculateTotalPlusTax(amt, tax, ttype);
+    }else {
+      return '';
+    }
+  }
+
+  onChanges(): void {
+    this.inputAmount.valueChanges.subscribe(
+      () => { 
+        this.showCalculatedTax();
+        this.showCalculatedTotal();
+      });
+   this.inputTax.valueChanges.subscribe(
+     () => {
+      this.showCalculatedTax();
+      this.showCalculatedTotal();
+     });
+   this.taxType.valueChanges.subscribe(
+     () => {
+      this.showCalculatedTax();
+      this.showCalculatedTotal();
+     });   
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModules } from './material.modules';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
